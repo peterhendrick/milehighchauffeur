1bf135f2c5bdac30e892a16e565af88b1c55e55b
Peter Hendrick <peterjhendrick@gmail.com>
2016-11-27 17:49:39 -0700

    fixed up the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ let gulp = require('gulp'),
     ghPages = require('gulp-gh-pages'),
     bower = require('gulp-bower'),
     clean = require('gulp-clean'),
+    peterGhRemote = 'https://github.com/peterhendrick/peterhendrick.github.io',
+    thomasGhRemote = 'https://github.com/ThomasHendrick/ThomasHendrick.github.io',
     sourceFiles = [
         './bower_components/**',
         './css/**',
@@ -27,26 +29,22 @@ gulp.task('clean', () => {
 
 gulp.task('deployPeter', ['bowerInstall', 'clean'], () => {
     return gulp.src(sourceFiles, {base: '.'})
-        .pipe(ghPages({
-            remoteUrl: 'https://github.com/peterhendrick/peterhendrick.github.io',
-            origin: 'origin',
-            branch: 'master',
-            cacheDir: 'dist/',
-            push: true,
-            force: true,
-            message: 'Gulp Deployment'
-        }))
+        .pipe(ghPages(_options(peterGhRemote)));
 });
 
 gulp.task('deploy', ['bowerInstall', 'clean'], () => {
     return gulp.src(sourceFiles, {base: '.'})
-        .pipe(ghPages({
-            remoteUrl: 'https://github.com/ThomasHendrick/ThomasHendrick.github.io',
-            origin: 'origin',
-            branch: 'master',
-            cacheDir: 'dist/',
-            push: true ,
-            force: true,
-            message: 'Gulp Deployment'
-        }))
+        .pipe(ghPages(_options(thomasGhRemote)));
 });
+
+function _options(remoteUrl) {
+    return {
+        remoteUrl: remoteUrl,
+        origin: 'origin',
+        branch: 'master',
+        cacheDir: 'dist/',
+        push: true,
+        force: true,
+        message: 'Gulp Deployment'
+    }
+}
