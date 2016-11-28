e3175aed6ab68699571b8092f6d1c2cf7001df52
Peter Hendrick <peterjhendrick@gmail.com>
2016-11-27 17:24:32 -0700

    got build and deploy working

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,39 @@
 'use strict';
 
-let gulp = require('gulp');
+let gulp = require('gulp'),
+    ghPages = require('gulp-gh-pages'),
+    bower = require('gulp-bower'),
+    clean = require('gulp-clean'),
+    sourceFiles = [
+        './bower_components/**/',
+        './css/**/*',
+        './images/**/*',
+        './js/**/*',
+        './pages/**/*',
+        '404.html',
+        'CNAME',
+        'favicon.ico',
+        'index.html'
+    ];
+
+gulp.task('clean', () => {
+    return gulp.src('dist/', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('bowerInstall', () => {
+    return bower();
+});
+
+gulp.task('deployPeter', ['bowerInstall', 'clean'], () => {
+    return gulp.src(sourceFiles, {base: '.'})
+        .pipe(ghPages({
+            remoteUrl: 'https://github.com/peterhendrick/peterhendrick.github.io',
+            origin: 'origin',
+            branch: 'master',
+            cacheDir: 'dist/',
+            push: true ,
+            force: true,
+            message: 'Gulp Deployment'
+        }))
+});
