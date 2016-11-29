acf05ea51abecdd11bb1dab21141693b8dc956b2
Peter Hendrick <peterjhendrick@gmail.com>
2016-11-28 22:31:10 -0700

    added done callback to jasmine tests

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ require('protractor');
 let url = process.env.URL_TO_TEST || 'http://localhost:8080';
 
 describe('milehighchauffeur.com', () => {
-    it('should display all elements on landing page', () => {
+    it('should display all elements on landing page', (done) => {
         browser.get(url);
         browser.waitForAngular();
 
@@ -21,71 +21,81 @@ describe('milehighchauffeur.com', () => {
         expect(element(by.id('airportLink')).isDisplayed()).toBeTruthy();
 
         expect(element(by.id('copyright')).isDisplayed()).toBeTruthy();
+        done();
     });
 
-    it('should navigate to airport link', () => {
+    it('should navigate to airport link', (done) => {
         element(by.id('airportLink')).click();
         browser.waitForAngular();
         browser.sleep(800);
         expect(element(by.id('airportPagePhoto')).isDisplayed()).toBeTruthy();
+        done();
     });
 
-    it('should navigate home by clicking the headerLogo', () => {
+    it('should navigate home by clicking the headerLogo', (done) => {
         element(by.id('headerLogo')).click();
         browser.waitForAngular();
         expect(element(by.id('home')).isDisplayed()).toBeTruthy();
+        done();
     });
 
-    it('should navigate to corporate travel link', () => {
+    it('should navigate to corporate travel link', (done) => {
         element(by.id('corporateTravelLink')).click();
         browser.waitForAngular();
         browser.sleep(800);
         expect(element(by.id('corporatePagePhoto')).isDisplayed()).toBeTruthy();
+        done();
     });
 
     _goHome();
 
-    it('should navigate to ski page link', () => {
+    it('should navigate to ski page link', (done) => {
         element(by.id('skiPageLink')).click();
         browser.waitForAngular();
         browser.sleep(800);
         expect(element(by.id('skiPagePhoto')).isDisplayed()).toBeTruthy();
+        done();
     });
 
-    it('should navigate to our services header link', () => {
+    it('should navigate to our services header link', (done) => {
         element(by.id('workBtn')).click();
         browser.waitForAngular();
         expect(element(by.id('home')).isDisplayed()).toBeTruthy();
+        done();
     });
 
-    it('should navigate to our cars link', () => {
+    it('should navigate to our cars link', (done) => {
         element(by.id('ourCarsBtn')).click();
         browser.waitForAngular();
         expect(element(by.id('mercedesPhoto')).isDisplayed()).toBeTruthy();
         expect(element(by.id('chevyPhoto')).isDisplayed()).toBeTruthy();
+        done();
     });
 
-    it('should navigate to about link', () => {
+    it('should navigate to about link', (done) => {
         element(by.id('aboutBtn')).click();
         browser.waitForAngular();
         expect(element(by.id('aboutTitle')).getText()).toEqual('About Mile High Chauffeur');
+        done();
     });
 
-    it('should open contact form', () => {
+    it('should open contact form', (done) => {
         element(by.id('contactBtn')).click();
         browser.waitForAngular();
         browser.sleep(500);
         expect(element(by.id('submitButton')).isDisplayed()).toBeTruthy();
+        done();
     });
 
-    it('should close contact form', () => {
+    it('should close contact form', (done) => {
         element(by.id('contactCloseButton')).click();
         browser.waitForAngular();
         browser.sleep(500);
         expect(element(by.id('submitButton')).isDisplayed()).toBeFalsy();
+        done();
     });
 
-    it('should navigate away from contact form', () => {
+    it('should navigate away from contact form', (done) => {
         element(by.id('contactBtn')).click();
         browser.waitForAngular();
         browser.sleep(500);
@@ -94,13 +104,15 @@ describe('milehighchauffeur.com', () => {
         browser.waitForAngular();
         browser.sleep(500);
         expect(element(by.id('submitButton')).isDisplayed()).toBeFalsy();
+        done();
     });
 
     // _goHome()
     function _goHome() {
-        it('should go home', () => {
+        it('should go home', (done) => {
             browser.get(url);
             browser.waitForAngular();
+            done();
         });
     }
 })
